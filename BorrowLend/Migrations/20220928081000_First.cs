ca80b04b9048e3949f0db7c116a3d7af34410e36
vikinacheva <vikinacheva04@gmail.com>
2022-09-28 12:17:43 +0300

    Add project files.

---
+++ BorrowLend/Migrations/20220928081000_First.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BorrowLend.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speedy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Borrower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BorrLender = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speedy", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speedy");
+        }
+    }
+}
---
+++ BorrowLend/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BorrowLend.Models
+{
+    [Table("Speedy")]
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ItemName { get; set; }
+
+        [Required]
+        public string Borrower { get; set; }
+
+        [Required]
+        public string BorrLender { get; set; }
+    }
+}
